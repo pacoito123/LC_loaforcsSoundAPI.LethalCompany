fcf2ffc073452410e84f6aba45e4abd685f18e73
Zeta Arcade <zZETAz@outlook.com>
2025-07-22 21:25:40 +0100

    Added a current Weather name condition

---
+++ loaforcsSoundAPI.LethalCompany/Conditions/Moon/CurrentWeatherCondition.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using loaforcsSoundAPI.Core.Data;
+using loaforcsSoundAPI.SoundPacks.Data.Conditions;
+[SoundAPICondition("LethalCompany:weather:name")] 
+public class WeatherNameCondition : Condition
+{
+    public string Value { get; internal set; }
+    public override bool Evaluate(IContext context)
+    {
+        if (!StartOfRound.Instance) return false;
+        if (!StartOfRound.Instance.currentLevel) return false;
+        string weatherName = StartOfRound.Instance.currentLevel.currentWeather.ToString();
+        return string.Equals(Value, weatherName, StringComparison.InvariantCultureIgnoreCase);
+    }
+}
